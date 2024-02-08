7023b735a838d775826f33eeb8d90280a9b8f51a
Facundo <facundobettella@outlook.com>
2024-02-08 17:35:21 -0300

    FB-PORTFOLIO. Add footer and fix in about section

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Facundo Developer",
+  title: "FacuDev 💻",
   description: "Portfolio",
 };
 
